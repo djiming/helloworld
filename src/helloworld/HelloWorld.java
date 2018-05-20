e456e78b841ee717c3f000350c0b4d900a669a8a
Djming <925449506@qq.com>
2018-05-21 03:24:20 +0800

    test

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+/*
+ * @author  djming
+ * @date 创建时间：2018年5月21日 上午3:11:40
+ * @throws
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
